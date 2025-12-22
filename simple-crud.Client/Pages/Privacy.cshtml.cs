c5c4ca646bbbd6afb5a631671f6ba575413a859a
andresglz01 <andresgilgonzalez@outlook.com>
2025-12-22 12:46:14 -0600

    Agregar archivos de proyecto.

---
+++ simple-crud.Client/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace simple_crud.Client.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
